65e24c40720c5233432ce88f309ed589bf41f768
tourl-ts <18312883857@163.com>
2022-09-18 23:28:48 +0800

    feat: 弹窗-1

--- my-tailc/src/components/Modal.vue
+++ my-tailc/src/components/Modal.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="modal" v-show="isShow">
-    <div class="modal-header"></div>
-    <div class="modal-container">
-      <template v-for="item in list" :key="item.id">
-        <span>{{ item.name }}</span>
-      </template>
+  <div class="modal z-10 border bg-slate-400 rounded-md" v-show="isShow">
+    <div class="flex flex-none flex-col flex-nowrap">
+      <div class="modal-header text-left text-black h-10 bg-white leading-10">
+        弹窗
+      </div>
+      <div class="modal-container">
+        <template v-for="item in list" :key="item.id">
+          <span class="block">{{ item.name }}</span>
+        </template>
+      </div>
+      <div class="modal-footer"></div>
     </div>
-    <div class="modal-footer"></div>
   </div>
+  <div
+    v-show="isShow"
+    class="mask z-5 bg-slate-400"
+    @click.stop="closeModal"
+  ></div>
 </template>
 
 <script setup lang="ts">
@@ -24,9 +33,22 @@ const activeShow = () => {
   isShow.value = !isShow.value;
 };
 
+const closeModal = () => {
+  isShow.value = false;
+};
+
 defineExpose({
   activeShow,
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal {
+  @apply modalcss;
+}
+.mask {
+  @apply modalcss;
+  width: 100%;
+  height: 100%;
+}
+</style>
