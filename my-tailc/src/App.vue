0d0be95b573bb13378645ec87b127fffbc39bf10
tourl-ts <18312883857@163.com>
2022-10-07 23:28:22 +0800

    feat: build打包构建

--- my-tailc/src/App.vue
+++ my-tailc/src/App.vue
@@ -1,42 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import Modal from "./components/Modal.vue";
-const modalRef = ref(Modal);
-const handleClick = () => {
-  modalRef.value.activeShow();
-};
+  console.log(import.meta.env);
 </script>
 
 <template>
-  <div class="flex justify-start">
-    <a href="https://vitejs.dev" class="flex-auto" target="_blank">
-      <img src="/vite.svg" class="logo w-full" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" class="flex-auto" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue w-full" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <button class="btn-indigo" @click="handleClick">Click me</button>
-  <Modal ref="modalRef" />
+  <router-view></router-view>
 </template>
 
-<style scoped>
-.btn-indigo {
-  @apply btn-blue;
-}
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  @apply card;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- my-tailc/vite.config.ts
+++ my-tailc/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname);
+  return {
+    plugins: [vue()],
+    base: env.VITE_MODE === "production" ? "./" : "/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      sourcemap: env.VITE_MODE === "development" ? true : false,
+    },
+  };
+});
