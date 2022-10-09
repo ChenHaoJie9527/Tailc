d92ddc43eba23cddb312e7d868d019ab6f58c4ee
tourl-ts <18312883857@163.com>
2022-10-10 01:48:27 +0800

    feat: 动态数据折线图

--- my-tailc/src/components/LineChart.vue
+++ my-tailc/src/components/LineChart.vue
@@ -6,14 +6,92 @@
 <script setup lang="ts">
 import { inject, onMounted } from "vue";
 import { lineChartStore } from "../store/lineChartStore";
-import { EchartsOptions, EchartsType } from "../echarts";
+import { EchartsType } from "../echarts";
 const echarts: EchartsType = inject("echarts")!;
 const store = lineChartStore();
 // 折线图
 const changeType = () => {
   // 获取组件实例
   const lineChart = echarts.init(document.getElementById("myChart")!);
-  lineChart.setOption(store.getStore()); // 复制
+
+  function randomData() {
+    now = new Date(+now + oneDay);
+    value = value + Math.random() * 21 - 10;
+    return {
+      name: now.toString(),
+      value: [
+        [now.getFullYear(), now.getMonth() + 1, now.getDate()].join("/"),
+        Math.round(value),
+      ],
+    };
+  }
+  let data: any[] = [];
+  let now = new Date(2022, 10, 10);
+  let oneDay = 24 * 3600 * 1000;
+  let value = Math.random() * 1000;
+  for (var i = 0; i < 1000; i++) {
+    data.push(randomData());
+  }
+  const options = {
+    title: {
+      text: "Dynamic Data & Time Axis",
+    },
+    tooltip: {
+      trigger: "axis",
+      formatter: function (params) {
+        params = params[0];
+        var date = new Date(params.name);
+        return (
+          date.getDate() +
+          "/" +
+          (date.getMonth() + 1) +
+          "/" +
+          date.getFullYear() +
+          " : " +
+          params.value[1]
+        );
+      },
+      axisPointer: {
+        animation: false,
+      },
+    },
+    xAxis: {
+      type: "time",
+      splitLine: {
+        show: false,
+      },
+    },
+    yAxis: {
+      type: "value",
+      boundaryGap: [0, "100%"],
+      splitLine: {
+        show: false,
+      },
+    },
+    series: [
+      {
+        name: "Fake Data",
+        type: "line",
+        showSymbol: false,
+        data: data,
+      },
+    ],
+  };
+
+  lineChart.setOption(options); // 复制
+  setInterval(function () {
+    for (var i = 0; i < 5; i++) {
+      data.shift();
+      data.push(randomData());
+    }
+    lineChart.setOption({
+      series: [
+        {
+          data: data,
+        },
+      ],
+    });
+  }, 1000);
   // 根据页面大小自动响应图标大小
   window.addEventListener("resize", () => {
     lineChart.resize();
--- my-tailc/src/store/lineChartStore.ts
+++ my-tailc/src/store/lineChartStore.ts
@@ -1,42 +1,89 @@
 import { defineStore } from "pinia";
 import { reactive, toRefs } from "vue";
-import type { EchartsOptions } from "../echarts";
+import type { EchartsOptions, EchartsType } from "../echarts";
 interface StateProp {
   chartData: EchartsOptions;
   [key: string]: any;
 }
+let data: any[] = [];
+let now = new Date(2022, 10, 10);
+let oneDay = 24 * 3600 * 1000;
+let value = Math.random() * 1000;
+function randomData() {
+  now = new Date(+now + oneDay);
+  console.log("now", now);
+  value = value + Math.random() * 21 - 10;
+  return {
+    name: now.toString(),
+    value: [
+      [now.getFullYear(), now.getMonth() + 1, now.getDate()].join("/"),
+      Math.round(value),
+    ],
+  };
+}
 export const lineChartStore = defineStore("lineChartStoreId", () => {
+  for (let i = 0; i < 1000; i++) {
+    data.push(randomData());
+  }
+
   const state = reactive<StateProp>({
     chartData: {
-      xAxis: {
-        data: ["A", "B", "C", "D", "E"],
+      title: {
+        text: "P&L",
       },
-      yAxis: {},
+      tooltip: {
+        trigger: "axis",
+        formatter: function (params) {
+          params = params[0];
+          const dates = new Date(params.name);
+          return (
+            dates.getFullYear() +
+            "/" +
+            (dates.getMonth() + 1) +
+            "/" +
+            dates.getDate() +
+            ":" +
+            params.value[1]
+          );
+        },
+      },
+      xAxis: {
+        type: "time",
+        splitLine: {
+          show: false,
+        },
+      },
+      yAxis: {
+        type: "value",
+        splitLine: {
+          show: false,
+        },
+        boundaryGap: [0, "100%"],
+      },
       series: [
         {
-          data: [10, 20, 30, 40, 50],
+          name: "P&L",
           type: "line",
-          stack: "x",
-        },
-        {
-          data: [5, 4, 3, 5, 10],
-          type: "line",
-          stack: "x",
-        },
-        {
-          data: [8, 7, 3, 10, 6],
-          type: "line",
-          stack: "x",
+          showSymbol: false,
+          data: data,
         },
       ],
-      tooltip: {
-        mainType: "tooltip",
-      },
     },
   });
+
+  const openInterval = () => {
+    setInterval(() => {
+      for (let i = 0; i < 5; i++) {
+        data.shift();
+        data.push(randomData());
+      }
+      state.chartData.series = [{ data }];
+    }, 5000);
+  };
   const getStore = () => state.chartData;
   return {
     ...toRefs(state),
     getStore,
+    openInterval,
   };
 });
